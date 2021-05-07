43f15c47f0d213aa9a6d7daebb60123a973914b3
Ramon Prata <c0009886@vli-logistica.com.br>
2021-05-07 11:32:15 -0300

    redux-setup old way

--- src/features/foo/redux/index.ts
+++ src/features/foo/redux/index.ts
@@ -1,6 +1,7 @@
 /**
  * Use this to export FEATURE actions, operations, reducer
  */
+
 export * from './FooOperations';
 export * from './FooActions';
 export { default as fooReducer } from './FooReducer';
